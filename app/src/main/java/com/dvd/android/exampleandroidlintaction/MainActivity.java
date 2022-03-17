a9e6a13d85237ef38b77b0bfb9c3f2d2f77b9a04
tatiana <tatiana.76@kakaostyle.com>
2022-03-17 15:20:53 +0900

    다른 브랜치에서 코드 수정 테스트

--- app/src/main/java/com/dvd/android/exampleandroidlintaction/MainActivity.java
+++ app/src/main/java/com/dvd/android/exampleandroidlintaction/MainActivity.java
@@ -33,6 +33,7 @@ public class MainActivity extends AppCompatActivity {
         appBarConfiguration = new AppBarConfiguration.Builder(navController.getGraph()).build();
         NavigationUI.setupActionBarWithNavController(this, navController, appBarConfiguration);
 
+        // 다른 브랜치에서 코드 수정
         binding.fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
